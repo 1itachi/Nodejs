967ecee19540f8ac4a725e60a803349eab2c7ddd
deepak <deepakk.svpuc@gmail.com>
2019-01-27 22:19:15 +0530

    updated server.js

--- node_todo_api/server/server.js
+++ node_todo_api/server/server.js
@@ -1,4 +1,5 @@
 var bodyParser = require('body-parser');
+var ObjectId   = require('mongodb').ObjectId;
 var express    = require('express');
 
 var mongoose = require('./db/mongoose').mongoose;
@@ -8,6 +9,7 @@ var users    = require('./models/users').user;
 var app = express();
 
 app.use(bodyParser.json());
+
 
 app.post('/todos', (req,res)=>{
   var todo = new Todo({
@@ -30,7 +32,19 @@ app.get('/todos', (req,res)=>{
 
 // '/todos/:id'
 app.get('/todos/:id', (req,res)=>{
-    res.send(req.params);
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)){
+        return  res.status(404).send('invalid id!');
+    }
+        Todo.findById(id).then((todo)=>{
+            if(!todo){
+              return  res.status(404).send('record not found!!');
+            }else{
+                return res.send(todo);
+            }
+        }).catch((e)=>{
+           res.status(400).send(e);
+        })  
 })
 
 app.listen(3000, ()=>{
